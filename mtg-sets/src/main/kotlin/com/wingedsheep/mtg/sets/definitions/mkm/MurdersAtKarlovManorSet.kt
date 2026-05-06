dfd91c1377b8fefc04a5953df7b31884f2f6706d
Bjørn-Olav Strand <bolav@ikke.no>
2026-05-06 20:18:00 +0200

    Escape Tunnel (MKM)

--- mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/mkm/MurdersAtKarlovManorSet.kt
+++ mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/mkm/MurdersAtKarlovManorSet.kt
@@ -16,6 +16,7 @@ object MurdersAtKarlovManorSet {
     val allCards = listOf(
         CommercialDistrict,
         ElegantParlor,
+        EscapeTunnel,
         HardHittingQuestion,
         HedgeMaze,
         LushPortico,
