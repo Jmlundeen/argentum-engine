9bfda35acdfb176b390cc25c76054fdd23a65aa6
vincent <wingedsheep89@gmail.com>
2026-05-08 20:58:00 +0200

    Color identities in json test.

--- mtg-sets/src/test/kotlin/com/wingedsheep/mtg/sets/RegenerateScourgeJson.kt
+++ mtg-sets/src/test/kotlin/com/wingedsheep/mtg/sets/RegenerateScourgeJson.kt
@@ -4,6 +4,7 @@ import com.wingedsheep.mtg.sets.definitions.scg.ScourgeSet
 import com.wingedsheep.sdk.serialization.CardExporter
 import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.FunSpec
+import java.nio.file.Files
 import java.nio.file.Path
 
 /**
@@ -18,7 +19,19 @@ import java.nio.file.Path
 class RegenerateScourgeJson : FunSpec({
     test("regenerate scourge json files") {
         val outputDir = Path.of("src/test/resources/cards/scourge/")
-        CardExporter.exportSet(ScourgeSet.cards, outputDir)
-        println("Regenerated ${ScourgeSet.cards.size} Scourge card JSON files to $outputDir")
+        val existing = Files.list(outputDir).use { stream ->
+            stream.map { it.fileName.toString() }.toList().toSet()
+        }
+        fun fileNameFor(name: String) = name
+            .lowercase()
+            .replace("'", "")
+            .replace(",", "")
+            .replace(" ", "-")
+            .replace("--", "-") + ".json"
+        val cardsToExport = ScourgeSet.cards.filter { card ->
+            fileNameFor(card.name) in existing
+        }
+        CardExporter.exportSet(cardsToExport, outputDir)
+        println("Regenerated ${cardsToExport.size} Scourge card JSON files to $outputDir")
     }
 })
