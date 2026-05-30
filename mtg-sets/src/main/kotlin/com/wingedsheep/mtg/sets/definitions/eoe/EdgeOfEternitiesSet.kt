728e778e299f85570e8b702639c52c667e614589
vincent <wingedsheep89@gmail.com>
2026-05-31 01:24:58 +0200

    Mark Edge of Eternities set as incomplete

--- mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/eoe/EdgeOfEternitiesSet.kt
+++ mtg-sets/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/eoe/EdgeOfEternitiesSet.kt
@@ -18,6 +18,7 @@ object EdgeOfEternitiesSet : MtgSet {
     override val displayName = "Edge of Eternities"
     override val releaseDate = "2025-08-01"
     override val sealedSupported = true
+    override val incomplete = true
 
     override val cards: List<CardDefinition> by lazy {
         CardDiscovery.findIn(CARDS_PACKAGE)
